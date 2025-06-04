4c855f1fb31fab86db185c0f440ec3ac9423059b
Kagankakao <kagankakao@gmail.com>
2025-06-04 11:56:47 +0300

    Add little changes

--- NeuralNotes/Backend/crud.py
+++ NeuralNotes/Backend/crud.py
@@ -95,8 +95,11 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
     return db_user
 
-def authenticate_user(db: Session, email: str, password: str):
-    user = get_user_by_email(db, email)
+def authenticate_user(db: Session, identifier: str, password: str):
+    user = get_user_by_email(db, identifier)
+    if not user:
+        user = get_user_by_username(db, identifier)
+
     if not user:
         return False
     if not security.verify_password(password, user.hashed_password):
